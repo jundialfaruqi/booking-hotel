1fabe13b59c230ff910371cd88205ebaf24ff620
jundialfaruqi <jundialfaruqi@gmail.com>
2025-08-01 17:46:20 +0700

    Layout

---
+++ app/about/page.tsx
@@ -0,0 +1,47 @@
+import Headersection from "@/components/header-section";
+import {IoEyeOutline, IoLocateOutline} from "react-icons/io5";
+import Image from "next/image";
+const AboutPage = () => {
+  return (
+    <div>
+      <Headersection title="About Us" subTitle="lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+      <div className="max-w-screen-xl mx-auto py-20 px-4">
+        <div className="grid md:grid-cols-2 gap-8">
+          <Image src="/about-image.jpg" alt="about image" width={650} height={579} />
+          <div>
+            <h1 className="text-5xl font-semibold text-gray-900 mb-4">Who We Are</h1>
+            <p className="text-gray-700 py-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione placeat earum dicta ex numquam culpa ea alias tenetur vel.
+            </p>
+            <ul className="list-item space-y-6 pt-8">
+              <li className="flex gap-5">
+                <div className="flex-none mt-1">
+                  <IoEyeOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Vision :</h4>
+                  <p className="text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ratione placeat earum dicta ex numquam culpa ea alias tenetur vel.
+                  </p>
+                </div>
+              </li>
+              <li className="flex gap-5">
+                <div className="flex-none mt-1">
+                  <IoLocateOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Mission :</h4>
+                  <p className="text-gray-600">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam inventore repudiandae, iure sequi dolor sapiente odit quae enim dignissimos! Omnis!
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavLink from "@/components/navbar/navlink";
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white shadow-sm z-20">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={128} height={49} priority />
+        </Link>
+        <NavLink />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
